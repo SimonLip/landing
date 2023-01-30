cbaf8f99d8fe86b1011f3fded9e395e6443f3da9
SimonLip <113662801+SimonLip@users.noreply.github.com>
2023-01-30 08:21:59 +0000

    js

---
+++ js/header-clock.js
@@ -0,0 +1,10 @@
+(function() {
+
+    function updateClock() {
+        const clockContainer = document.querySelector('.header__clock');
+        clockContainer.innerText = new Date().toLocaleTimeString('uk');
+    }
+
+    setInterval(updateClock, 1000);
+
+})();
